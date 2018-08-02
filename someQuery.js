7da42800ae4b1d6698ce8050d726bb7a1ebb09be
Luddendorf <luddendorf2@yahoo.com>
2018-08-02 14:12:24 +0300

    Update someQuery.js

--- someQuery.js
+++ someQuery.js
@@ -2,3 +2,18 @@
 jQuery(function($) {
 
  $('input:focus');
+
+ $('.grid_12 h2')
+    .queue('newanima', function() {
+      $(this).animate({'fontSize':'100px'});
+      $(this).dequeue('newanima');
+ })
+    .delay(1000, 'newanima')
+    .queue('newanima', function() {
+  
+      $(this).text('New age');
+      $(this).dequeue('newanima');
+ });
+ 
+ $('.grid_12 h2).dequeue('newanima');
+      
