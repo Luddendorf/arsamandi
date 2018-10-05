8d656705a1bf60ec0861e012043880755df666cd
Luddendorf <luddendorf2@yahoo.com>
2018-10-05 11:14:32 +0300

    Create forms-tdd.ts

---
+++ forms-tdd.ts
@@ -0,0 +1,30 @@
+export class AppModule {
+  
+  onSubmit() {
+    
+  }
+  
+}
+
+
+// app.component.html ////////////////
+
+<form>
+
+<label for="username">Username</label>
+<input type="text" id="username"
+       name="username"
+       ngModel>
+       
+<label>Mail</label>
+<input type="email"
+       name="email"
+       ngModel>
+       
+<select name="secret"
+        ngModel
+><select>
+
+<button typeon>
+
+</form>
