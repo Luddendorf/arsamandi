67f30df33d2b891a6b34b780b0f499ebe04a90b3
Luddendorf <luddendorf2@yahoo.com>
2018-10-19 17:40:32 +0300

    Update ssr-rendering.ts

--- packages/ssr-rendering.ts
+++ packages/ssr-rendering.ts
@@ -161,6 +161,128 @@ app.listen(3000, () => {
 
 deploy : dist, dist-server, package.json, server.js 
 
+import { Component, trigger, state, style, transition, animate } from '@angular/core';
+
+@Component({
+  selector: 'app-root',
+  templateUrl: './app.component.html',
+  animations: [
+    trigger('divState', [
+      state('normal', style({
+        'background-color': 'red',
+        transform: 'translateX(0)'
+      })),
+      state('high', style({
+        'background-color': 'blue',
+        transform: 'translateX(100px)'
+      })),
+      transition('normal => high', animate(300)),
+      transition('high => normal', animate(800))
+    ])
+  ]
+})
+
+export class AppComponent {
+
+  state = 'high';
+
+  list = ['a', 'b', 'c'];
+  
+  onAnimate() {
+    this.state == 'normal' ? this.state = 'high' : this.state = 'normal';
+  }
+  
+  onAdd(item) {
+    this.list.push(item);
+  }
+  
+  onDelete(item) {
+    this.list.splice(this.list.indexOf(item), 1);
+  }
+}
+animations: [
+  trigger('mySuperTrigger', [
+    state('firstState', style({
+      'background-color': 'red',
+      transform: 'translateX(0)'
+    })),
+    state('secondState', style({
+      'background-color': 'blue',
+      transform: 'translateX(100px)'
+    }))
+  ])
+]
+
+animations: [
+   trigger('myTrigOne', [
+     state('normal', style({
+       'background-color': 'red'
+     })),
+     state('high', style({
+       'background-color': 'blue'
+     })),
+     transition('normal <=> high', animate(800))
+   ])
+]
+trigger('wildTrigger', [
+  state('begin', style({
+    transform: 'translateX(0) scale(1)'
+  })),
+  state('middle', style({
+    transform: 'translateX(100px) scale(1)'
+  })),
+  state('end', style({
+    transform: 'translateX(0) scale(0.5)'
+  }))
+  transition('begin => middle', animate(300)),
+  transition('middle => begin', animate(800)),
+  transition('end <=> *', [
+    style({
+       'background-color': 'orange'
+    }),
+    animate(1000, style({
+       borderRadius: '50px'
+    })),
+    animate(500)
+  ])
+])
+
+onAnimate() {
+  
+  this.wildState == 'begin' ? this.wildState = 'middle' : this.wildState = 'begin';
+  
+  this.wildState == 'begin' ? this.wildState = 'middle' : this.wildState = 'begin';
+  
+}
+
+onShrink() {
+
+  this.wildState = 'end';
+}
+
+@Component({
+  animations: [
+    trigger('photoState', [
+      state('move', style({
+        transform: 'translateX(-100%) translateY(50px)',
+      })),
+      state('enlarge', style({
+        transform: 'scale(1.5)',
+      })),
+      state('spin', style({
+        transform: 'rotateY(180deg) rotateZ(90deg)'
+      })),
+      transition('spin => move', animate('2000ms ease-out')),
+      transition('* => move', animate('500ms', keyframes([
+   style({transform: 'translateX(0) rotateY(0),
+  
+      ])
+    ])
+  ]
+})
+
+
+
 
 
 
