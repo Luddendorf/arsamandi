f11c41392916f13a9d55aedc6b49d97b5f0566cb
Luddendorf <luddendorf2@yahoo.com>
2018-11-06 15:43:18 +0200

    Update motto-design.ts

--- ngrx-routing/motto-design.ts
+++ ngrx-routing/motto-design.ts
@@ -230,3 +230,186 @@ myFunction() {
 // index.js ////////////////////////////////////////
 export * from './store';
 export * from './reducers';
+
+// reducers.ts /////////////////////////////////////////
+import * as fromActions from './actions';
+
+export const initialState = {
+    loaded: false,
+    loading: false,
+    data: [{label: 'Eat pizza', complete: false}]
+};
+
+export function reducer(state = initialState,
+                        action: {type: string, payload: any}) {
+   switch(action.type) {
+       case fromActions.ADD_INGREDIENTS: {
+         const todo = action.payload;
+         const newData = [...state.data, todo];
+         return {
+             ...state,
+             data: newData
+         };
+       }
+           
+       case fromActions.DELETE_INGREDIENTS: {
+          const data = state.data.filter(
+   todo => todo.label !== action.payload.label);
+           
+           return {
+           ...state,
+           data
+         };
+       }
+   }
+    
+    return state;
+}
+
+
+
+
+
+//  app.ts ////////////////////////////////////////////
+import * as fromStore from './store';
+
+import { renderTodos } from './utils';
+
+const destroy = document.querySelector('.unsubscribe') as HTMLButtonElement;
+
+const reducers = {
+   todos: fromStore.reducer;
+ };
+
+const store = new fromStore.Store(reducers);
+
+button.addEventListener( 
+  'click',
+  () => {
+      if(!input.value.trim()) return;
+
+  const myNewData = { label: input.value, complete: false };
+    
+   store.dispatch(new fromStore.AddIngredients(myNewData));
+      
+   input.value = '';
+}, false
+);
+
+ const unsubscribe = store.subscribe(state => {
+    
+    renderTodos(state.todos.data);
+ });
+
+ destroy.addEventListener('click', unsubscribe, false);
+ 
+ todoList.addEventListener('click', function(event) {
+   const target = event.target as HTMLButtonElement;
+     
+   if(target.nodeName.toLowerCase() === 'button') {
+       
+       const todo = JSON.parse(target.getAttribute('data-todo') as any);
+       store.dispatch(new fromStore.DeleteIngredients(todo));
+   }
+ });
+
+ store.subscribe(state => console.log('STATE:::', state));
+
+
+// store.ts ///////////////////////////////////////////
+export class Store {
+    private subscribers: Function[];
+    private reducers: { [key: string]: Function };
+    private state: { [key: string]: any }
+    
+  constructor(reducers = {}, initialState = {}) {
+     
+     this.subscribers = [];
+     this.reducers = reducers;
+     this.state = this.reduce(initialState, {});
+  }
+    
+  get value() {
+      return this.state;
+  }
+    
+  subscribe(fn) {
+     this.subscribers = [...this.subscribers, fn];
+     
+     this.notify();
+      
+     return () => {
+       this.subscribers = this.subscribers.filter(sub => sub !== fn)
+     }
+  }
+    
+  dispatch(action) {
+      this.state = this.reduce(this.state, action);
+      
+      this.notify();
+  }
+    
+  private notify() {
+      this.subscribers.forEach(fn => fn(this.value));
+  }
+    
+    
+  private reduce(state, action) {
+    
+    const newState = {};
+      
+    for(const prop in this.reducers) {
+       newState[prop] = this.reducers[prop](state[prop], action);
+    }
+      
+    return newState;
+  }
+}
+
+// utils.ts ///////////////////////////////
+
+const span = document.querySelector('span') as HTMLSpanElement;
+const todoList = document.querySelector('.todos') as HTMLElement;
+
+export function renderTodos(collection) {
+    span.innerHTML = collection.length;
+    
+    todoList.innerHTML = '';
+    
+    for(const item of collection) {
+        todoList.innerHTML += `
+          <li>
+            ${item.label}
+            <button type="button" data-todo='${JSON.stringify(item)}'>
+Delete
+            </button>
+          </li>
+`
+    }
+}
+
+
+// actions.ts //////////////////////////////////////
+// action constants
+export const ADD_INGREDIENTS = '[General] Add Ingredients';
+export const DELETE_INGREDIENTS = '[General] Delete Ingredients';
+
+// action creators
+export class AddIngredients {
+    readonly type = ADD_INGREDIENTS;
+    
+    constructor(private payload: any) {}
+}
+
+export class DeleteIngredients {
+    readonly type = DELETE_INGREDIENTS;
+    
+    constructor(private payload: any) {}
+}
+
+
+
+// index.ts //////////////////
+export * from './store';
+export * from './reducers';
+export * from './actions';
