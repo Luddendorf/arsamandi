eee507c769c7b989c7d0a58da6c20e7c0e3d7154
Luddendorf <luddendorf2@yahoo.com>
2018-11-07 17:50:32 +0200

    Update motto-design3-entities.ts

--- ngrx-routing/motto-design3-entities.ts
+++ ngrx-routing/motto-design3-entities.ts
@@ -129,3 +129,72 @@ export const getAllPizzas = createSelector(
    }
 );
 
+// app/store/reducers/index.ts ///////////////////////////////////
+import { ActivatedRouteSnapshot, RouterStateSnapshot, Params } from '@angular/router';
+import { ActionReducerMap, createFeatureSelector } from '@ngrx/store';
+
+import * as fromRouter from '@ngrx/router-store';
+
+export interface RouterStateUrl {
+  url: string;
+  queryParams: Params;
+  params: Params;
+}
+
+export interface State {
+    routerReducer: fromRouter.RouterReducerState<RouterStateUrl>
+}
+
+export const reducers: ActionReducerMap<State> = {
+    routerReducer: fromRouter.routerReducer
+};
+
+export const getRouterState = createFeatureSelector<
+  fromRouter.RouterReducerState<RouterStateUrl>
+      >('routerReducer');
+
+
+// custom serializer ///////////////
+export class CustomSerializer implements fromRouter.RouterStateSerializer<RouterStateUrl> {
+   
+ serialize(routerState: RouterStateSnapshot): RouterStateUrl {
+        
+   const { url } = routerState;
+     
+   const { queryParams } = routerState.root;
+     
+   let state: ActivatedRouteSnapshot = routerState.root;
+   
+   while(state.firstChild) {
+      state = state.firstChild;
+   } 
+   
+   const {params} = state.params;  
+     
+   return { url, queryParams, params };
+ }
+}
+
+
+
+
+// app/store/index.ts //////////////////////////////////////////
+export * from './reducers';
+
+
+// app.module.ts ///////////////////////////////////////////
+import { StoreRouterConnectingModule,
+  RouterStateSerializer } from '@ngrx/router-store';
+
+import { reducers, CustomSerializer } from './store';
+
+
+@NgModule({
+    imports: [
+       StoreModule.forRoot(reducers, { metaReducers }),
+       StoreRouterConnectingModule,
+       
+    ],
+    providers: [{provide: RouterStateSerialize,
+               useClass: CustomSerializer}],
+})
