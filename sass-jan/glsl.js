61bff498755f36b6ba7c78e4f0b9741eb4d3bc56
Luddendorf <luddendorf2@yahoo.com>
2019-03-08 13:36:29 +0200

    Update glsl.js

--- sass-jan/glsl.js
+++ sass-jan/glsl.js
@@ -18,9 +18,20 @@ void main() {
     
     float p1 = p - 1.0;
     // to make distortions:
+    
+    vec2 position = step(0.0, p) * uv + step(0.0, -p) * (1.0 - uv);
+    
     float dx1 = p * 0.8;
+    
+    float vert = abs(p * 0.3);
+    
+    dx1 -= step(0.2 - vert, position.x / 2.0) * 0.3 * p;
+    dx1 -= step(0.4 - vert, position.x / 2.0) * 0.3 * p;
+    dx1 += step(0.6 - vert, position.x / 2.0) * 0.3 * p;
+    dx1 += step(0.8 - vert, position.x / 2.0) * 0.3 * p;
+    
     // TEXTURE 1 : ////////////////////////////////////////////////////////////
-    vec4 tex1 = texture2D(texture1, vec2(vUv1.x + p, vUv1.y));
+    vec4 tex1 = texture2D(texture1, vec2(vUv1.x + dx1, vUv1.y));
     
     
     // float bounds = step(0.0, p) * uv + step(0.0, -p) * (1.0, -uv);
@@ -31,14 +42,27 @@ void main() {
     
     
    // WE MAKE SECOND TEXTURE: //////////////////////////////////////////
-   vec4 tex2 = texture2D(texture2, vec2(vUv1.x + p, vUv1.y));
+   float dx2 = p1*0.8;
     
-   float bounds1 = step(0.0, 1.0 - (uv.x/2.0 + p1)) * step(0.0, uv.x/2 + p);
+   float vert = abs(p*0.3);
+    
+   dx2 -= step(0.2 - vert1, position.x / 2.0) * 0.3 * p1;
+   dx2 -= step(0.4 - vert1, position.x / 2.0) * 0.3 * p1;
+   dx2 += step(0.6 - vert1, position.x / 2.0) * 0.3 * p1;
+   dx2 += step(0.8 - vert1, position.x / 2.0) * 0.3 * p1;
+   
+   // we replace p1 with dx2! :
+   vec4 tex2 = texture2D(texture2, vec2(vUv1.x + dx2, vUv1.y));
+  
+   
+   float bounds1 = step(0.0, 1.0 - (uv.x/2.0 + p1)) * step(0.0, uv.x/2 + p1);
+  
    
    fcolor += tex2*bounds1;
    
     
    gl_FragColor = fcolor;
+    
 }
 
 
