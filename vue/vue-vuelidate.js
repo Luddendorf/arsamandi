1131981d0db7e8376a7ffc0279ef10de83c630f5
Luddendorf <luddendorf2@yahoo.com>
2019-04-03 23:54:53 +0300

    Update vue-vuelidate.js

--- vue/vue-vuelidate.js
+++ vue/vue-vuelidate.js
@@ -97,3 +97,95 @@ new Vue({
     background-color: #ffc9aa;
   }
 </style>
+
+<!-- npm install --save vuelidate -->
+
+<template>
+	<form @submit.prevent="onSubmit">
+	<div class="input" :class="{invalid: $v.email.$error}">
+		<label for="email">Mail</label>
+		<input type="email"
+		       id="email"
+		       @blur="$v.email.$touch()"
+		       v-model="email">
+   <p v-if="!$v.email.email">Please provide a valid email address.</p>
+   <p v-if="!$v.email.required">This field is required.</p>
+	</div>
+	<div class="input" :class="{invalid: $v.age.$error}">
+		<label for="age">Your Age</label>
+		<input type="number"
+		       id="age"
+		       @blue="$v.age.$touch()"
+		       v-model.number="age">
+		<p v-if="!$v.age.minVal">You have to be at least {{ $v.age.$params.minVal.min }}</p>
+	</div>
+	<div class="input" :class="{invalid: $v.password.$error}">
+		<label for="password">Password</label>
+		<input type="password"
+		       id="password"
+		       @blur="$v.password.$touch()"
+		       v-model="password">
+	</div>
+	<div class="input" :class="{invalid: $v.password.$error}">
+		<label for="confirm-password">Confirm Password</label>
+		<input type="password"
+		       id="confirm-password"
+		       @blur="$v.confirmPassword.$touch()"
+		       v-model="confirmPassword">
+	</div>
+  </form>
+</template>
+
+<script>
+  import { required, email, numeric, minValue, minLength,
+   sameAs } from 'vuelidate/lib/validators';
+
+	export default {
+		data() {
+			return {
+        email: '',
+        age: null,
+        password: '',
+        confirmPassword: '',
+        country: 'usa',
+        hobbyInputs: [],
+        terms: false
+			};
+		},
+		validations: {
+     email: {
+      required: required,
+      email: email
+     },
+     age: {
+     	required: required,
+      numeric: numeric,
+      minVal: minValue(18)
+     },
+     password: {
+     	required: required,
+     	minLen: minLength(6)
+     },
+     confirmPassword: {
+      sameAs: sameAs('password')
+      sameAs: sameAs(vm => {
+      	return vm.password + 'b';
+      })
+     }
+		},
+		methods: {
+
+		}
+	}
+</script>
+
+<style>
+	.input.invalid input {
+		border: 1px solid red;
+		background-color: #ffc9aa;
+	}
+
+	.input.invalid label {
+		color: red;
+	}
+</style>
